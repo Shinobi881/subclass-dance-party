91043a642cb124d75a78b11f05274fcdebd2d099
Shinobi881 <moose.techguy@gmail.com>
2015-07-08 19:37:18 -0700

    Make Carlton Dancer Complete

---
+++ src/carltonDancer.js
@@ -0,0 +1,11 @@
+var makeCarltonDancer = function(top, left, timeBetweenSteps){
+	makeDancer.apply(this, arguments);
+
+	this.$node = $('<img src="https://33.media.tumblr.com/2dfc3369827df9b981e111d7fd8fc732/tumblr_mvemcyarmn1rslphyo1_400.gif" class="dancer noborder">');
+	this.setPosition(top, left);
+};
+
+makeCarltonDancer.prototype = Object.create(makeDancer.prototype);
+makeCarltonDancer.prototype.constructor = makeCarltonDancer;
+
+makeCarltonDancer.prototype.step = function(){}; //no step animation because carlton has already achieved perfection.
